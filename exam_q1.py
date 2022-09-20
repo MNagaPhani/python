a45783a5976b14c3d61d6251c1083ac4ca5c60fa
MNagaPhani <113904927+MNagaPhani@users.noreply.github.com>
2022-09-20 10:05:13 +0530

    Create exam_q1.py

---
+++ exam_q1.py
@@ -0,0 +1,13 @@
+year=2040
+if(year%400==0 and year%100==0):
+    print("The year ",year," is a leap year")
+elif(year%4==0 and year%100!=0):
+    print("The year ",year," is a leap year")
+else:
+    print("The year ",year," is not a leap year")
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Securit
